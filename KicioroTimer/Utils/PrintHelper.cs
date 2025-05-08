72331aa565fc1392e1c9a0e376ef8340c5b1188b
kuro <smocks-ham-14@icloud.com>
2025-05-08 23:03:55 +0200

    time format

---
+++ KicioroTimer/Utils/PrintHelper.cs
@@ -0,0 +1,14 @@
+namespace KicioroTimer.Utils;
+
+public static class PrintHelper
+{
+    public static string PrintTime(TimeSpan span)
+    {
+        return string.Format("{0}h{1}m", span.Days * 24 + span.Hours, span.Minutes);
+    }
+    
+    public static string PrintHours(TimeSpan span)
+    {
+        return string.Format("{0}h", span.Days * 24 + span.Hours);
+    }
+}
